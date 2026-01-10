9f78805a0a2d7ada488615b8f83c0c4fa05644e6
AashishkBishi <aswinibishi02@gmail.com>
2026-01-10 19:15:44 +0530

    Selection-Sort.py
    Implement selection sort algorithm to sort a list.

---
+++ Selection-Sort.py
@@ -0,0 +1,13 @@
+#selection-sort Algorithm
+
+l=[6,2,9,4,0,6]     
+for ind1 in range(len(l)-1):
+    leng=ind1
+    for ind2 in range(ind1+1,len(l)):
+        if l[leng] > l[ind2]:
+            leng=ind2
+    l[ind1],l[leng]=l[leng],l[ind1]
+print(l)
+
+
+output: [0, 2, 4, 6, 6, 9]
