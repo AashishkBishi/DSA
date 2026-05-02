75a4450c392b3ecb343b5b93719735f995b2bb11
AashishkBishi <aswinibishi02@gmail.com>
2026-05-02 12:00:19 +0530

    binary search

--- Binary-search.py
+++ Binary-search.py
@@ -6,7 +6,7 @@ while least <high:
     mid = (least+high)//2
     if k[mid] > target:
         high = mid-1
-    if k[mid] < target:
+    elif k[mid] < target:
         least = mid+1
     else:
         print(mid)
